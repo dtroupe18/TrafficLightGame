7b40790febc71967ab8d672e3bb8da15d35c7670
Dave <Dave.Troupe@gmail.com>
2018-05-24 18:10:50 -0400

    Initial Commit

---
+++ TrafficLightGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrafficLightGame
+//
+//  Created by Dave on 5/24/18.
+//  Copyright © 2018 High Tree Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TrafficLightGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrafficLightGame
+//
+//  Created by Dave on 5/24/18.
+//  Copyright © 2018 High Tree Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
